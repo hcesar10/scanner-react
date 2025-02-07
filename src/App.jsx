c1e5f20d325feb30e1fbee5a55b426a6508e9dd9
Henrique Cesar <henrique.cesar@democrata.com.br>
2025-02-07 14:09:51 -0300

    teste aparelho

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,20 @@ const App = () => {
   };
 
   return (
-    <div style={{ textAlign: 'center', height: '100px', width: '100px', backgroundColor: 'red' }}>
-      <input type="text" />
-      {/* <Html5QrcodePlugin
+    <main>
+      <body>
+        <div style={{ textAlign: 'center', height: '100px', width: '100px', backgroundColor: 'red' }}>
+          <input type="text" />
+          {/* <Html5QrcodePlugin
         fps={10}
         qrbox={250}
         disableFlip={false}
         qrCodeSuccessCallback={onNewScanResult}
       />
       {status} */}
-    </div>
+        </div>
+      </body>
+    </main>
   );
 };
 
