0d958dde4992954a4d0787740b09d8d992911ed3
Henrique Cesar <henrique.cesar@democrata.com.br>
2025-02-07 15:02:57 -0300

    add scanner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Html5QrcodePlugin from './Html5QrcodePlugin';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log(`Scan result: ${decodedText}`, decodedResult);
+    setData(decodedText);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Html5QrcodePlugin
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          qrCodeSuccessCallback={onNewScanResult}
+        />
+        {data}
         <a
           className="App-link"
           href="https://reactjs.org"
