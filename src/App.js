694717a1891ac4bbf44d676d7b03298690eca3ff
Henrique Cesar <henrique.cesar@democrata.com.br>
2025-02-17 10:55:00 -0300

    mudança sistema para toast result

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Html5QrcodePlugin from './Html5QrcodePlugin';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   const [data, setData] = useState('');
@@ -9,18 +10,50 @@ function App() {
   const onNewScanResult = (decodedText, decodedResult) => {
     console.log(`Scan result: ${decodedText}`, decodedResult);
     setData(decodedText);
+    if (decodedText.endsWith("7")) {
+      notifySuccess(decodedText);
+    } else notifyError(decodedText)
   };
+
+  const notifySuccess = (text) => toast.success(text, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored'
+  });
+
+  const notifyError = (text) => toast.error(text, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored'
+  });
+
 
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" onKeyUp={(e) => {
-          if (e.key === 'Enter') {
-            setData(e.target.value);
-            e.target.value = '';
-          }
-        }} />
+        <input
+          type="textarea"
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              setData(e.target.value);
+              if (e.target.value.endsWith("7")) {
+                notifySuccess(e.target.value);
+              } else notifyError(e.target.value)
+              e.target.value = '';
+            }
+          }}
+        />
         <Html5QrcodePlugin
           fps={10}
           qrbox={250}
@@ -28,7 +61,8 @@ function App() {
           qrCodeSuccessCallback={onNewScanResult}
         />
         {data}
-      </header>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
