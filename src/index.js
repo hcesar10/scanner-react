45c04d0d994ba409ee95d3c03f9bd0f377c2cd5b
Henrique Cesar <henrique.cesar@democrata.com.br>
2025-02-25 08:51:39 -0300

    turn app into PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
 root.render(
   <React.StrictMode>
     <App />
